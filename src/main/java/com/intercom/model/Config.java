5cb2c4ac7a7d63caf7c3112c53d2875400e8f311
Philip Lemmermann <philip.lemmermann@icloud.com>
2022-11-24 18:41:30 +0100

    Removed unused code in config.

--- src/main/java/com/intercom/model/Config.java
+++ src/main/java/com/intercom/model/Config.java
@@ -17,89 +17,6 @@ public class Config {
     public Config() {
     }
 
-    public void createDefaults() {
-        getRooms().clear();
-        getActions().clear();
-
-        Room philip = new Room();
-        philip.setId("philip");
-        philip.setName("Philip");
-        philip.setHostName("pi-philip.local");
-        getRooms().add(philip);
-
-        Room jule = new Room();
-        jule.setId("jule");
-        jule.setName("Jule");
-        jule.setHostName("pi-jule.local");
-        getRooms().add(jule);
-
-        Room armin = new Room();
-        armin.setId("armin");
-        armin.setName("Armin");
-        armin.setHostName("pi-armin.local");
-        getRooms().add(armin);
-
-        Room kitchen = new Room();
-        kitchen.setId("kitchen");
-        kitchen.setName("Küche");
-        kitchen.setHostName("pi-kitchen.local");
-        getRooms().add(kitchen);
-
-        Room office = new Room();
-        office.setId("office");
-        office.setName("Büro");
-        office.setHostName("pi-office.local");
-        getRooms().add(office);
-
-        Action duty = new Action();
-        duty.setId("duty");
-        duty.setName("Ämtli");
-        getActions().add(duty);
-
-        Action sleep = new Action();
-        sleep.setId("sleep");
-        sleep.setName("Schlafen");
-        getActions().add(sleep);
-
-        Action come = new Action();
-        come.setId("come");
-        come.setName("Bitte kommen");
-        getActions().add(come);
-
-        Action homework = new Action();
-        homework.setId("homework");
-        homework.setName("Hausaufgaben");
-        getActions().add(homework);
-
-        Action teeth = new Action();
-        teeth.setId("teeth");
-        teeth.setName("Zähneputzen");
-        getActions().add(teeth);
-
-        Action shower = new Action();
-        shower.setId("shower");
-        shower.setName("Duschen");
-        getActions().add(shower);
-
-        philip.getAvailableActions().add(come.getId());
-        jule.getAvailableActions().add(come.getId());
-        armin.getAvailableActions().add(come.getId());
-
-        kitchen.getAvailableActions().add(duty.getId());
-        kitchen.getAvailableActions().add(sleep.getId());
-        kitchen.getAvailableActions().add(come.getId());
-        kitchen.getAvailableActions().add(shower.getId());
-        kitchen.getAvailableActions().add(teeth.getId());
-        kitchen.getAvailableActions().add(homework.getId());
-
-        office.getAvailableActions().add(duty.getId());
-        office.getAvailableActions().add(sleep.getId());
-        office.getAvailableActions().add(come.getId());
-        office.getAvailableActions().add(shower.getId());
-        office.getAvailableActions().add(teeth.getId());
-        office.getAvailableActions().add(homework.getId());
-    }
-
     public List<Room> getRooms() {
         return rooms;
     }
@@ -176,16 +93,5 @@ public class Config {
     public static Config load(boolean testing) {
         Gson gson = new GsonBuilder().create();
         return gson.fromJson(new InputStreamReader(Config.class.getResourceAsStream(testing ? "config-testing.json" : "config.json"), StandardCharsets.UTF_8), Config.class);
-    }
-
-    private void save() {
-        createDefaults();
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        System.out.println(gson.toJson(this));
-    }
-
-    public static void main(String[] args) {
-        Config config = new Config();
-        config.save();
     }
 }
